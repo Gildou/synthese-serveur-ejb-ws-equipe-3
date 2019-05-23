b75de09ed0176169e8fb52627852840e96998f15
IN-MP-028 <javajee2k19@gmail.com>
2019-05-23 14:16:29 +0200

    Push DaoImpl v1.0

--- src/com/infotel/dao/DaoImpl.java
+++ src/com/infotel/dao/DaoImpl.java
@@ -3,110 +3,152 @@ package com.infotel.dao;
 import java.util.List;
 
 import javax.ejb.LocalBean;
-import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import com.infotel.metier.Personne;
-import com.infotel.metier.Voiture;
+import com.infotel.metier.Magasin;
+import com.infotel.metier.Produit;
+import com.infotel.metier.ProduitNonPerissable;
+import com.infotel.metier.ProduitPerissable;
 
 @Stateless
-@LocalBean	//afin que la dao puisse etre accessible par le WS
+@LocalBean // afin que la dao puisse etre accessible par le WS
 public class DaoImpl implements IdaoLocal, IdaoRemote {
 
 	@PersistenceContext(unitName = "pu_ejb_ws")
 	EntityManager em;
-	
-	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Ajouter +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	// ++++++++++++++++++++AJOUTER++++++++++++++++++++
+
 	@Override
-	public void addPersonne(Personne p) {
-		// TODO Auto-generated method stub
+	public void addMagasin(Magasin m) {
+		em.persist(m);
+	}
+
+	@Override
+	public void ajouterProduitNonPerissable(ProduitNonPerissable pnp) {
+		em.persist(pnp);
+	}
+
+	@Override
+	public void ajouterProduitPerissable(ProduitPerissable pp) {
+		em.persist(pp);
+	}
+	
+	@Override
+	public void ajouterProduit(Produit p, long idMagasin) {
+		Magasin m = new Magasin();
+		p.setMagasin(m);
 		em.persist(p);
 	}
 
+	// +++++++++++++++++++SUPPRIMER++++++++++++++++++++
+
+
 	@Override
-	public void addVoiture(Voiture v, long idPersonne) {
-		// TODO Auto-generated method stub
-		Personne p = em.find(Personne.class, idPersonne);
-		v.setPersonne(p);
-		em.persist(v);
+	public void supprimerMagasin(Magasin m) {
+
+		em.remove(m);
 	}
 
-	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Supprimer +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	@Override
-	public long supprimerPersonne(long idPersonne) {
+	public long supprimerProduitNonPerissable(long idProduit) {
 		Query q = null;
 		try {
-			q = em.createQuery("DELETE FROM Personne p where p.id = :idp").setParameter("idp", idPersonne);
-			idPersonne = q.executeUpdate();
+			q = em.createQuery("DELETE FROM Produit pnp where p.idProduit = :idpnp").setParameter("idpnp", idProduit);
+			idProduit = q.executeUpdate();
 
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
-		return idPersonne;
+		return idProduit;
 	}
-	
+
 	@Override
-	public long supprimerVoiture(long idVoiture) {
+	public long supprimerProduitPerissable(long idProduit) {
 		Query q = null;
 		try {
-			q = em.createQuery("DELETE FROM Voiture p where p.idVoiture = :idv").setParameter("idv", idVoiture);
-			idVoiture = q.executeUpdate();
+			q = em.createQuery("DELETE FROM Produit pp where p.idProduit = :idpp").setParameter("idpp", idProduit);
+			idProduit = q.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return idProduit;
+	}
+
+	// +++++++++++++++++++MODIFIER++++++++++++++++++++
+
+	@Override
+	public void modifierMagasin(Magasin m) {
+		em.merge(m);
+	}
+
+	@Override
+	public void modifierProduitNonPerissable(ProduitNonPerissable pnp) {
+		em.merge(pnp);
+	}
+
+	@Override
+	public void modifierProduitPerissable(ProduitPerissable pp) {
+		em.merge(pp);
+	}
+
+	// +++++++++++++++++++GET++++++++++++++++++++
+
+	@Override
+	public Magasin getMagasin(long idMagasin) {
+		return em.find(Magasin.class, idMagasin);
+	}
+
+	@Override
+	public ProduitNonPerissable getProduitNonPerissable(long idProduit) {
+		return em.find(ProduitNonPerissable.class, idProduit);
+	}
+
+	@Override
+	public ProduitPerissable getProduitPerissable(long idProduit) {
+		return em.find(ProduitPerissable.class, idProduit);
+	}
+
+	@Override
+	public List<Magasin> getAllMagasin() {
+		return em.createQuery("SELECT m FROM Magasin m").getResultList();
+	}
+
+	@Override
+	public List<Produit> getAllProduit() {
+		return em.createQuery("SELECT p FROM Produit p").getResultList();
+	}
+
+	@Override
+	public List<ProduitNonPerissable> getAllProduitNonPerissable() {
+		return em.createQuery("SELECT p FROM Produit p where TYPE_PROD=ProdNonPeri").getResultList();
+	}
+
+	@Override
+	public List<ProduitPerissable> getAllProduitPerissable() {
+		return em.createQuery("SELECT p FROM Produit p where TYPE_PROD=ProdPeri").getResultList();
+	}
+
+	@Override
+	public List<Produit> getProduitParMagasin(long idMagasin) {
+		return em.createQuery("SELECT p FROM Produit p WHERE p.magasin.idMagasin = :idMagasin")
+				.setParameter("idMagasin", idMagasin).getResultList();
+	}
+
+	// +++++++++++++++++++CUSTOM++++++++++++++++++++
+	
+	@Override
+	public double calculPrixMagasin(Magasin m) {
+		double prixP=0;
+		for(Produit p : m.getProduits()) {
+			prixP = prixP + p.getPrix()*p.getStock();
+		}
 			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return idVoiture;
+		return (prixP + m.getPrixDuLocal());
 	}
-	
-	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Modifier +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	@Override
-	public void modifierPersonne(Personne p) {
-		// TODO Auto-generated method stub
-		em.merge(p);
-	}
-
-	@Override
-	public void modifierVoiture(Voiture v) {
-		// TODO Auto-generated method stub
-		em.merge(v);
-	}
-	
-	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Lecture +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	@Override
-	public Personne getPersonne(long idPersonne) {
-		// TODO Auto-generated method stub
-		return em.find(Personne.class, idPersonne);
-	}
-
-	@Override
-	public Voiture getVoiture(long idVoiture) {
-		// TODO Auto-generated method stub
-		return em.find(Voiture.class, idVoiture);
-	}
-		
-	@Override
-	public List<Personne> getAllPersonne() {
-		// TODO Auto-generated method stub 
-		return em.createQuery("SELECT p FROM Personne p").getResultList();
-	}
-
-	@Override
-	public List<Voiture> getAllVoiture() {
-		// TODO Auto-generated method stub
-		return em.createQuery("SELECT v FROM Voiture v").getResultList();
-	}
-
-	@Override
-	public List<Voiture> getVoitureParPersonne(long idPersonne) {
-		// TODO Auto-generated method stub
-		return em.createQuery("SELECT v FROM Voiture v WHERE v.personne.id = :idPersonne").setParameter("idPersonne", idPersonne).getResultList();
-	}
-
-	
 
 }
