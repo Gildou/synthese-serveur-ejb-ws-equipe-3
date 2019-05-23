c1227cae3cddaf75e986a756da2d2d3cec8a876b
Adam PROUST <adam.proust@outlook.fr>
2019-05-23 11:17:04 +0200

    maj metier

--- src/com/infotel/metier/ProduitPerissable.java
+++ src/com/infotel/metier/ProduitPerissable.java
@@ -1,5 +1,28 @@
 package com.infotel.metier;
 
-public class ProduitPerissable {
+import java.util.Date;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("ProdPeri")
+public class ProduitPerissable extends Produit{
+
+	private Date dateLimiteUtilisation;
+
+	public Date getDateLimiteUtilisation() {
+		return dateLimiteUtilisation;
+	}
+
+	public void setDateLimiteUtilisation(Date dateLimiteUtilisation) {
+		this.dateLimiteUtilisation = dateLimiteUtilisation;
+	}
+
+	@Override
+	public String toString() {
+		return "ProduitPerissable [dateLimiteUtilisation=" + dateLimiteUtilisation + "]";
+	}
+	
+	
 }
